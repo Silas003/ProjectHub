7304c29d1f4ef0b07f190af16de90527b5d7d670
owbird <owbirdphorccuo@gmail.com>
2024-05-09 08:42:26 +0000

    feat (Project List): Add browse all btn

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import ProjectCard from "./ProjectCard";
 
 interface ProjectListProps {
@@ -8,7 +9,15 @@ interface ProjectListProps {
 const ProjectList = ({ title, projects }: ProjectListProps) => {
   return (
     <div className="mt-8 ml-4">
-      <h1 className="font-bold text-lg ml-4">{title}</h1>
+      <div className="flex justify-between mr-4">
+        <h1 className="font-bold text-lg ml-4">{title}</h1>
+        <Link
+          href="/projects"
+          className="border border-secondary p-4 hover:bg-primary hover:text-white rounded-sm"
+        >
+          Browse all
+        </Link>
+      </div>
       <hr className="ml-4 w-60 h-4 bg-secondary" />
 
       <div className="grid grid-cols-3">
