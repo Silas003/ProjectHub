6ea829c8595a78b91296ee78b87726f7d543a7b8
owbird <owbirdphorccuo@gmail.com>
2024-05-14 17:32:59 +0000

    feat: Show all projects

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,24 @@
+import Navbar from "@/components/Navbar";
+import ProjectList from "@/components/ProjectList";
+
+export default function Projects() {
+  const projects: Project[] = new Array(100).fill(0).map((id) => ({
+    id,
+    title: "The next twitter",
+    thumbnail:
+      "https://nickjanetakis.com/assets/blog/cards/how-to-start-and-finish-any-web-app-project-678900795cfd6d4fa60e3655dd62ae9f61ef5e14b62ca62050e817e43e861f11.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt viverra nulla non venenatis. Nullam sed nunc egestas, mollis diam iaculis, aliquet magna. Curabitur varius egestas sem sed bibendum. Donec a facilisis tortor. Phasellus ullamcorper tincidunt dictum. Aenean venenatis, metus ac molestie pharetra, nisi purus tempor orci, ut porta magna sem non velit. Donec ac rhoncus mauris. Aliquam non ante non turpis tincidunt interdum sit amet sit amet arcu. In sollicitudin non orci vitae tristique. Mauris interdum et nulla vel condimentum. In commodo velit eu diam pretium, ut elementum leo pellentesque. Vestibulum sodales purus sed accumsan pretium.",
+    year: 2020 + id,
+    student: "Dope Khid",
+    type: ["mini", "final", "personal"][
+      Math.floor(Math.random() * 3)
+    ] as Project["type"]
+  }));
+
+  return (
+    <main className="">
+      <Navbar />
+      <ProjectList showHeader={false} title="Featured projects" projects={projects} />
+    </main>
+  );
+}
--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -4,21 +4,30 @@ import ProjectCard from "./ProjectCard";
 interface ProjectListProps {
   projects: Project[];
   title: string;
+  showHeader?: boolean;
 }
 
-const ProjectList = ({ title, projects }: ProjectListProps) => {
+const ProjectList = ({
+  title,
+  projects,
+  showHeader = true
+}: ProjectListProps) => {
   return (
     <div className="mt-8 ml-4">
-      <div className="flex justify-between mr-4">
-        <h1 className="font-bold text-lg ml-4">{title}</h1>
-        <Link
-          href="/projects"
-          className="border border-secondary p-4 hover:bg-primary hover:text-white rounded-sm"
-        >
-          Browse all
-        </Link>
-      </div>
-      <hr className="ml-4 w-60 h-4 bg-secondary" />
+      {showHeader && (
+        <div>
+          <div className="flex justify-between mr-4">
+            <h1 className="font-bold text-lg ml-4">{title}</h1>
+            <Link
+              href="/projects"
+              className="border border-secondary p-4 hover:bg-primary hover:text-white rounded-sm"
+            >
+              Browse all
+            </Link>
+          </div>
+          <hr className="ml-4 w-60 h-4 bg-secondary" />
+        </div>
+      )}
 
       <div className="grid grid-cols-3">
         {projects.map((project) => (
